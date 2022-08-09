41b120b662691bfe56c95d2401bfec3c4065a085
MahediBD <mhdiptoucm@gmail.com>
2022-08-09 17:30:53 +0600

    number comit

--- 07-number.php
+++ 07-number.php
@@ -18,3 +18,18 @@ var_dump($x);
 $x = acos(8);
 var_dump($x);
 ?>
+
+
+<?php
+// Cast float to int
+$x = 23465.768;
+$int_cast = (int)$x;
+echo $int_cast;
+
+echo "<br>";
+
+// Cast string to int
+$x = "23465.768";
+$int_cast = (int)$x;
+echo $int_cast;
+?>
