5b9aa27e15d58332ff0dc39b74c08f97d4d68f18
MahediBD <mhdiptoucm@gmail.com>
2022-08-09 17:26:24 +0600

    string comit

---
+++ 06-string.php
@@ -0,0 +1,19 @@
+<!-- word count -->
+<?php
+echo str_word_count("Hello world!"); 
+?>
+
+<!-- reverse string -->
+<?php
+echo strrev("Hello world!"); 
+?>
+
+<!-- text search within string -->
+<?php
+echo strpos("Hello world!", "world"); 
+?>
+
+<!-- string replace -->
+<?php
+echo str_replace("world", "Dolly", "Hello world!"); // outputs Hello Dolly!
+?>
