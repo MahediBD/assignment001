40a27e2d9322665c0d5670f0e1d3828690552ee3
MahediBD <mhdiptoucm@gmail.com>
2022-08-09 17:29:42 +0600

    number comit

--- 07-number.php
+++ 07-number.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>number</title>
-</head>
-<body>
+
 <?php
 $x = 5985;
 var_dump(is_int($x));
@@ -14,6 +6,15 @@ var_dump(is_int($x));
 $x = 59.85;
 var_dump(is_int($x));
 ?>
+
+<!-- infinty -->
+<?php
+$x = 1.9e411;
+var_dump($x);
+?>
     
-</body>
-</html>
+<!-- nun -->
+<?php
+$x = acos(8);
+var_dump($x);
+?>
